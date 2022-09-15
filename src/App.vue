b8e723948af228e5f6eb7156f4ce172a98f230b6
Alfin Noviaji <alpinnz@gmail.com>
2022-09-15 09:39:48 +0700

    cok

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <button onclick="flutterChannel({'type':'to','action':'nama routing'})">{'type':'to','action':'nama routing'}</button>
     
     <button onclick="flutterChannel({'string sajaaa'})">'string sajaaa'</button>
@@ -9,12 +8,10 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
